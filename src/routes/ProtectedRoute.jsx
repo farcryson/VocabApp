92de70749dc64394431cd1f321e86e5042c59fd8
Varshith <o.varshith5454@gmail.com>
2025-08-05 01:32:13 +0530

    authentication error fixed

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,13 +1,19 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "../context/AuthContext";
+import { use } from "passport";
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useContext(AuthContext);
   const backend = import.meta.env.VITE_BACKEND_URL;
 
+  useEffect(() => {
+    if (!loading && !user) {
+      window.location.href = `${backend}/auth/google`;
+    }
+  }, [loading, user]);
+
   if (loading) return <div>Loading...</div>;
   if (!user) {
-    window.location.href = `${backend}/auth/google`;
     return null;
   }
   return children;
